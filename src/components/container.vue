19fb1ba29a6463092e665467785b3a815cc3b90d
Rori Kurniadi <rori.kurniadi@yahoo.com>
2016-04-17 20:50:17 +0700

    add login page

--- src/components/container.vue
+++ src/components/container.vue
@@ -1,17 +1,24 @@
-
 <template>
-  <app-header></app-header>
-  <app-menu></app-menu>
-  <app-content></app-content>
-  <app-footer></app-footer>
+  <div v-if="!login">
+    <app-header></app-header>
+    <app-menu></app-menu>
+    <app-content></app-content>
+    <app-footer></app-footer>
+  </div>
+
+  <div v-if="login">
+    <app-login></app-login>
+  </div>
+
 </template>
 
 <script>
-  
   module.exports = {
     name : 'Container',
     data: function(){
       return {
+        login: true,
+        body_class: "hold-transition login-page", 
         menus: require('./../../config/menus.js') || []
       }
     },
@@ -20,7 +27,8 @@
       'app-header' : require('./layout/header.vue'),
       'app-menu': require('./layout/menu.vue'),
       'app-content': require('./layout/content.vue'),
-      'app-footer': require('./layout/footer.vue')
+      'app-footer': require('./layout/footer.vue'),
+      'app-login': require('./login.vue')
     }
   }
   
